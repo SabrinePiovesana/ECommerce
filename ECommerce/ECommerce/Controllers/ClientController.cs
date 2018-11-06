e2be6d640199eef1c48d8673f772f85bc2d4b22d
Sabrine Piovesana <sabry.piovesana@gmail.com>
2018-11-06 21:42:08 -0200

    .

---
+++ ECommerce/ECommerce/Controllers/ClientController.cs
@@ -0,0 +1,37 @@
+﻿using ECommerce.DataAccess;
+using ECommerce.Model;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace ECommerce.Controllers
+{
+    public class ClientController : Controller
+    {
+        private DataContext db = new DataContext();
+
+        // GET: Client
+        public ActionResult Index()
+        {
+            return View(db.Clients.ToList());
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,Name")] Client client)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Clients.Add(client);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(client);
+        }
+    }
+}
