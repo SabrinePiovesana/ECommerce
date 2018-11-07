5d5c8c1b7647538632079bd901e7ef840064ffaf
Sabrine Piovesana <sabry.piovesana@gmail.com>
2018-11-07 06:47:13 -0200

    Client

--- ECommerce/ECommerce/Controllers/ClientController.cs
+++ ECommerce/ECommerce/Controllers/ClientController.cs
@@ -1,15 +1,17 @@
 ﻿using ECommerce.DataAccess;
 using ECommerce.Model;
+using System.Data.Entity;
 using System.Linq;
+using System.Net;
 using System.Web.Mvc;
 
 namespace ECommerce.Controllers
 {
+    [RoutePrefix("client")]
     public class ClientController : Controller
     {
-        private DataContext db = new DataContext();
+        private readonly DataContext db = new DataContext();
 
-        // GET: Client
         public ActionResult Index()
         {
             return View(db.Clients.ToList());
@@ -32,6 +34,70 @@ namespace ECommerce.Controllers
             }
 
             return View(client);
+        }
+
+        [Route("detalhes-{id}")]
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            Client client = db.Clients.FirstOrDefault(x=>x.ID == id);
+            if (client == null)            
+                return HttpNotFound();
+            
+            return View(client);
+        }
+
+        [Route("editar-{id}")]
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)            
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+
+            Client client = db.Clients.FirstOrDefault(x => x.ID == id);
+            if (client == null)            
+                return HttpNotFound();
+            
+            return View(client);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [Route("editar-{id}")]
+        public ActionResult Edit([Bind(Include = "ID,Name")] Client client)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(client).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(client);
+        }
+
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)            
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            
+            Client client = db.Clients.FirstOrDefault(x => x.ID == id);
+            if (client == null)            
+                return HttpNotFound();
+            
+            return View(client);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Client client = db.Clients.FirstOrDefault(x => x.ID == id);
+            db.Clients.Remove(client);
+            db.SaveChanges();
+            return RedirectToAction("Index");
         }
     }
 }
